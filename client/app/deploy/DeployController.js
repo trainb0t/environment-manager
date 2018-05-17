23df1a421e2385cea2321dea305a37beb4ae2277
Gavin van der Merwe <gavin.vandermerwe@thetrainline.com>
2018-05-17 16:02:30 +0100

    Think we have bottomed out what we want the UI to do

--- client/app/deploy/DeployController.js
+++ client/app/deploy/DeployController.js
@@ -5,41 +5,72 @@
 'use strict';
 
 angular.module('EnvironmentManager.deploy').controller('DeployController',
-  function ($scope, $routeParams, $location, $uibModal, $http, $q, modal, 
-    resources, cachedResources, Environment, localstorageservice, teamstorageservice, 
+  function ($scope, $routeParams, $location, $uibModal, $http, $q, modal,
+    resources, cachedResources, Environment, localstorageservice, teamstorageservice,
     WizardHandler, portservice) {
-    
+
     var vm = this;
-    
+
     vm.bluePort = 40000;
     vm.greenPort = 41000;
     vm.applicationID = 0;
     vm.owningClusters = [];
     vm.createNewDeploymentMap = true;
-    vm.deploymentMaps = ['PlatformServices'];
-    vm.selectedServerRoleType = 'Linux';
+
+    vm.deploymentMaps = [{
+      Name: 'Cluster',
+      Roles: ['Service C.A', 'Service C.B', 'Service C.C'],
+    }, {
+      Name: 'Staging',
+      Roles: ['Service S.A', 'Service S.B', 'Service S.C'],
+    },
+    {
+      Name: 'Production',
+      Roles: ['Service P.A', 'Service P.B', 'Service P.C']
+    }];
+
+    vm.selectedDeploymentMaps = [];
+
     vm.serverRoleTypes = ['Linux', 'Windows', 'Custom'];
+    vm.selectedServerRoleType = 'Linux';
+
+    vm.serverRoleSizeTypes = ['Small', 'Medium', 'Large'];
+    vm.selectedServerRoleSizeType = 'Medium';
 
     function init() {
-      portservice.getNextSequentialPair().then(function(portPair){
+      portservice.getNextSequentialPair().then(function (portPair) {
         vm.bluePort = portPair.Blue;
         vm.greenPort = portPair.Green;
       });
-      cachedResources.config.clusters.all().then(function(clusters){
+      cachedResources.config.clusters.all().then(function (clusters) {
         vm.owningClusters = _.map(clusters, 'ClusterName').sort();
       });
     }
 
+    // TODO: This is rubbish and should not be here. Please see rubbish bindings in deploymentmap-details.html
+    vm.findDeploymentMapByName = function(deploymentMapName){
+      return vm.deploymentMaps.find(function (map) {
+        return map.Name === deploymentMapName;
+      });
+    }
 
-    vm.finishedWizard = function(){
+    vm.addDeploymentMap = function () {
+      vm.selectedDeploymentMaps.push({ DeploymentMap: 'new Map', ServerRole: '' })
+    }
+
+    vm.removeDeploymentMap = function (index) {
+      vm.selectedDeploymentMaps.splice(index, 1);
+    }
+
+    vm.finishedWizard = function () {
       console.log('Wizard finished ... ');
     };
 
-    vm.cancelledWizard = function(){
+    vm.cancelledWizard = function () {
       console.log('Wizard cancelled ... ');
     };
 
-    $scope.$on('wizard:stepChanged', function(event, args) {
+    $scope.$on('wizard:stepChanged', function (event, args) {
       console.log('Step changed ... ');
       console.log(args);
     });
